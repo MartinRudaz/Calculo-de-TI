0999bcd8b2c2ba66ad313896c4a7aa6d3c7bafea
MartinRudaz <66872196+MartinRudaz@users.noreply.github.com>
2026-03-06 16:37:32 -0300

    Mejora UI y nombres de variables del cálculo

--- Backend/TI.py
+++ Backend/TI.py
@@ -2,106 +2,126 @@ import math
 
 
 def calculate_transformer_values(
-    txt: float,
-    txt2: float,
-    txt3: float,
-    txt4: float,
-    txt5: float,
-    txt6: float,
-    txt7: float,
-    txt8: float,
-    txt9: float,
-    txt10: float,
-    txt11: float,
-    txt12: float,
+    prestacion_va: float,
+    corriente_primaria_a: float,
+    corriente_secundaria_a: float,
+    factor_seguridad_fs: float,
+    frecuencia_hz: float,
+    factor_apilado: float,
+    diametro_conductor_mm: float,
+    diametro_interno_mm: float,
+    espiras_nucleo: float,
+    factor_potencia: float,
+    ancho_fleje_mm: float,
+    apilado_mm: float,
 ) -> dict:
-    if not -1 <= txt10 <= 1:
-        raise ValueError("PF (txt10) debe estar entre -1 y 1")
-    if txt3 == 0 or txt5 == 0 or txt9 == 0:
-        raise ValueError("txt3, txt5 y txt9 deben ser distintos de 0")
+    if not -1 <= factor_potencia <= 1:
+        raise ValueError("El factor de potencia debe estar entre -1 y 1")
+    if corriente_secundaria_a == 0 or frecuencia_hz == 0 or espiras_nucleo == 0:
+        raise ValueError("Corriente secundaria, frecuencia y espiras deben ser distintas de 0")
 
-    senphi = math.sin(math.acos(txt10))
-    txt9irasteoricas = txt2 / txt3
+    seno_phi = math.sin(math.acos(factor_potencia))
+    espiras_teoricas = corriente_primaria_a / corriente_secundaria_a
 
-    vueltas_por_capa = int(txt8 * 3.14 / (txt7 + 0.5))
+    vueltas_por_capa = int(diametro_interno_mm * 3.14 / (diametro_conductor_mm + 0.5))
     if vueltas_por_capa <= 0:
         raise ValueError("La geometría produce 0 vueltas por capa")
 
-    capas = math.ceil(txt9irasteoricas / txt9 / vueltas_por_capa)
+    cantidad_capas = math.ceil(espiras_teoricas / espiras_nucleo / vueltas_por_capa)
 
-    diam_int = int(txt8 - (2 * (capas * (txt7 + 0.5))))
-    diam_ext = int(txt8 + (2 * txt12) + (2 * (capas * (txt7 + 0.5))))
-    diametro_medio_nucleo = txt8 + (0.7 + txt7 + 0.12 + 0.5) * 2 + txt12
-    longitud = diametro_medio_nucleo * 3.14159 / 1000
-    desarrollo_medio_alambre = (txt11 + txt12) * 2 + 0.7 * 4 + (txt7 + 0.12) * 2
-    seccion_alambre = (txt7**2) * 3.14159 / 4
-    resistenciaconductor = 0.0172 * desarrollo_medio_alambre * txt9irasteoricas / txt9 / seccion_alambre / 1000
+    diametro_interno_resultado = int(diametro_interno_mm - (2 * (cantidad_capas * (diametro_conductor_mm + 0.5))))
+    diametro_externo_resultado = int(diametro_interno_mm + (2 * apilado_mm) + (2 * (cantidad_capas * (diametro_conductor_mm + 0.5))))
+    diametro_medio_nucleo = diametro_interno_mm + (0.7 + diametro_conductor_mm + 0.12 + 0.5) * 2 + apilado_mm
+    longitud_medio_nucleo_m = diametro_medio_nucleo * 3.14159 / 1000
+    desarrollo_medio_alambre_mm = (ancho_fleje_mm + apilado_mm) * 2 + 0.7 * 4 + (diametro_conductor_mm + 0.12) * 2
+    seccion_alambre_mm2 = (diametro_conductor_mm**2) * 3.14159 / 4
+    resistencia_conductor_ohm = (
+        0.0172 * desarrollo_medio_alambre_mm * espiras_teoricas / espiras_nucleo / seccion_alambre_mm2 / 1000
+    )
 
-    tension_secundaria = txt / txt3
-    area_nucleo_cm2 = (txt11 * txt12 * txt6) * 10 ** (-2)
-    induccion = tension_secundaria / txt9 / 4.44 / txt5 / area_nucleo_cm2 * 10**4
+    tension_secundaria_v = prestacion_va / corriente_secundaria_a
+    area_nucleo_cm2 = (ancho_fleje_mm * apilado_mm * factor_apilado) * 10 ** (-2)
+    induccion_tesla = tension_secundaria_v / espiras_nucleo / 4.44 / frecuencia_hz / area_nucleo_cm2 * 10**4
 
-    if induccion > 0.5:
-        h = 11.852 * induccion**2 - 4.026 * induccion + 9.1573
+    if induccion_tesla > 0.5:
+        intensidad_campo_h = 11.852 * induccion_tesla**2 - 4.026 * induccion_tesla + 9.1573
     else:
-        h = 15.49 * induccion**0.6309
+        intensidad_campo_h = 15.49 * induccion_tesla**0.6309
 
-    if induccion < 0.163:
-        ctecorr = 794.39 * induccion**4 - 566.01 * induccion**3 + 138.27 * induccion**2 - 13.63 * induccion + 1.7174
-    elif induccion < 1.0977:
-        ctecorr = 1.279
+    if induccion_tesla < 0.163:
+        constante_correccion = (
+            794.39 * induccion_tesla**4
+            - 566.01 * induccion_tesla**3
+            + 138.27 * induccion_tesla**2
+            - 13.63 * induccion_tesla
+            + 1.7174
+        )
+    elif induccion_tesla < 1.0977:
+        constante_correccion = 1.279
     else:
-        ctecorr = (
-            -62.4817 * induccion**6
-            + 482.9707 * induccion**5
-            - 1523.8608 * induccion**4
-            + 2512.1181 * induccion**3
-            - 2284.8936 * induccion**2
-            + 1088.765 * induccion
+        constante_correccion = (
+            -62.4817 * induccion_tesla**6
+            + 482.9707 * induccion_tesla**5
+            - 1523.8608 * induccion_tesla**4
+            + 2512.1181 * induccion_tesla**3
+            - 2284.8936 * induccion_tesla**2
+            + 1088.765 * induccion_tesla
             - 211.3421
         )
 
-    io = h * longitud / txt9irasteoricas / ctecorr * 1.1
-    errorimag = io / txt3 * 100
-    area_nucleo_m2 = txt11 * txt12 * txt6 * 10 ** (-6)
-    peso_chapa = area_nucleo_m2 * longitud * 7.65 * 1000
+    corriente_magnetizacion_a = intensidad_campo_h * longitud_medio_nucleo_m / espiras_teoricas / constante_correccion * 1.1
+    error_imaginario_pct = corriente_magnetizacion_a / corriente_secundaria_a * 100
+    area_nucleo_m2 = ancho_fleje_mm * apilado_mm * factor_apilado * 10 ** (-6)
+    peso_chapa_kg = area_nucleo_m2 * longitud_medio_nucleo_m * 7.65 * 1000
 
-    if induccion > 0.15:
-        corrientedeperdidas = (
+    if induccion_tesla > 0.15:
+        corriente_perdidas_a = (
             (
-                0.1429 * induccion**6
-                - 0.4415 * induccion**5
-                + 0.4754 * induccion**4
-                - 0.1727 * induccion**3
-                + 0.2914 * induccion**2
-                + 0.0652 * induccion
+                0.1429 * induccion_tesla**6
+                - 0.4415 * induccion_tesla**5
+                + 0.4754 * induccion_tesla**4
+                - 0.1727 * induccion_tesla**3
+                + 0.2914 * induccion_tesla**2
+                + 0.0652 * induccion_tesla
                 - 0.006
             )
-            * peso_chapa
-            / tension_secundaria
+            * peso_chapa_kg
+            / tension_secundaria_v
         )
     else:
-        corrientedeperdidas = 0
+        corriente_perdidas_a = 0
 
-    erroripderdidas = corrientedeperdidas / txt3 * 100
-    corrientetotalcircmag = (io**2 + corrientedeperdidas**2) ** 0.5
-    errordemodulo_a = (-(errorimag * senphi + erroripderdidas * txt10)) + (txt9irasteoricas / txt9 - 1) * 100
-    errorde_angulo = (ctecorr * txt10 - corrientetotalcircmag * senphi) * 20
-    induccion_a_txt4 = tension_secundaria * txt4 / txt9irasteoricas / txt9 / 4.44 / txt5 / area_nucleo_cm2 * 10**4
-    consumo_interno = (txt3**2) * resistenciaconductor
+    error_perdidas_pct = corriente_perdidas_a / corriente_secundaria_a * 100
+    corriente_total_circuito_magnetico_a = (corriente_magnetizacion_a**2 + corriente_perdidas_a**2) ** 0.5
+    error_modulo_pct = (
+        -(error_imaginario_pct * seno_phi + error_perdidas_pct * factor_potencia)
+        + (espiras_teoricas / espiras_nucleo - 1) * 100
+    )
+    error_angulo_min = (constante_correccion * factor_potencia - corriente_total_circuito_magnetico_a * seno_phi) * 20
+    induccion_a_fs_tesla = (
+        tension_secundaria_v
+        * factor_seguridad_fs
+        / espiras_teoricas
+        / espiras_nucleo
+        / 4.44
+        / frecuencia_hz
+        / area_nucleo_cm2
+        * 10**4
+    )
+    consumo_interno_va = (corriente_secundaria_a**2) * resistencia_conductor_ohm
 
     return {
-        "pesochapa": peso_chapa,
-        "Induccion": induccion,
-        "Io": io,
-        "errorimag": errorimag,
-        "errordemoduloA": errordemodulo_a,
-        "errordeAngulo": errorde_angulo,
-        "ConsumoInterno": consumo_interno,
-        "H": h,
-        "Longitud": longitud,
-        "ctecorr": ctecorr,
-        "DiamInt": diam_int,
-        "Diamext": diam_ext,
-        "Induccionatxt4": induccion_a_txt4,
+        "pesochapa": peso_chapa_kg,
+        "Induccion": induccion_tesla,
+        "Io": corriente_magnetizacion_a,
+        "errorimag": error_imaginario_pct,
+        "errordemoduloA": error_modulo_pct,
+        "errordeAngulo": error_angulo_min,
+        "ConsumoInterno": consumo_interno_va,
+        "H": intensidad_campo_h,
+        "Longitud": longitud_medio_nucleo_m,
+        "ctecorr": constante_correccion,
+        "DiamInt": diametro_interno_resultado,
+        "Diamext": diametro_externo_resultado,
+        "Induccionatxt4": induccion_a_fs_tesla,
     }
